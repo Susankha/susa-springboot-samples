a96bdd75c3067a4a58eb96d6df4706cc9a8a7f95
Susankha <susankhanirmala@gmail.com>
2024-10-25 10:43:30 +0530

    Revert "enabled log4j2 support"

--- SpringSample/src/main/java/com/susa/sample/DemoApplication.java
+++ SpringSample/src/main/java/com/susa/sample/DemoApplication.java
@@ -5,8 +5,6 @@ import com.susa.sample.controller.EmployeeController;
 import com.susa.sample.repository.EmployeeRepository;
 import com.susa.sample.service.EmployeeService;
 import com.susa.sample.service.EmployeeServiceImpl;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,13 +12,9 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DemoApplication {
 
-  private static final Logger logger = LogManager.getLogger(DemoApplication.class);
-
   public static void main(String[] args) {
     ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
     String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-    logger.info("****** log4j2 logger ***********");
-
 
     EmployeeController ec = applicationContext.getBean(EmployeeController.class);
     System.out.println(" *********** ec " + ec.hashCode());
